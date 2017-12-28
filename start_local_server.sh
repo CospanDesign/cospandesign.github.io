e9ed0d55e2756a762541c86ae392d28ae1907c12
cospan <dave.mccoy@cospandesign.com>
2017-12-28 07:12:07 -0500

    CHANGE - Updated server script to use installed Jekyll

--- start_local_server.sh
+++ start_local_server.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "If this does not run install Jekyll"
+
 echo "Start local server on port 8080"
-bundle exec jekyll serve --host 0.0.0.0 -P 8080
+jekyll serve --host 0.0.0.0 --server 8080
 
