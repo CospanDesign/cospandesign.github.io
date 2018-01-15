72bfc2866850631f15df22073578aed5a36b4115
cospan <cospan@gmail.com>
2018-01-15 17:58:08 -0500

    Fixed local server script

--- start_local_server.sh
+++ start_local_server.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#! /bin/bash
 
 echo "If this does not run install Jekyll"
 
 echo "Start local server on port 8080"
-jekyll serve --host 0.0.0.0 --server 8080
+jekyll server --host 0.0.0.0 --port 8080
 
